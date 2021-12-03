95686e13c4de22170458b29c465e5535eb225255
ram-davezz <54266874+ram-davezz@users.noreply.github.com>
2021-12-03 14:55:29 +0530

    Create sidebar.php

---
+++ include/sidebar.php
@@ -0,0 +1,56 @@
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <img src="assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+           style="opacity: .8">
+      <span class="brand-text font-weight-light">AdminPage</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="assets/dist/img/avatar5.png" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Admin</a>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item has-treeview menu-open">
+            <ul class="nav nav-treeview">
+            <li class="nav-item">
+                <a href="index1.php" class="nav-link active">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>SEARCH</p>
+                </a>
+              <li class="nav-item">
+                <a href="view.php" class="nav-link active">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>ADD USERS</p>
+                </a>
+              </li>
+              
+             
+              </li>
+              <li class="nav-item">
+                <a href="logout.php" class="nav-link active">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>LOGOUT</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
